e180768539a7243b85e3ff184d728803ad1be8cc
Joao <bakeosso1@gmail.com>
2026-08-11 15:25:18 -0300

    arrumando a application

--- Pizzaria.Application/Interfaces/IPizzaService.cs
+++ Pizzaria.Application/Interfaces/IPizzaService.cs
@@ -12,7 +12,7 @@ namespace Pizzaria.Application.Interfaces
         Task<IEnumerable<PizzaDto>> GetFeaturedAsync();
         Task<IEnumerable<PizzaDto>> GetByCategoryAsync(int categoryId);
         Task<PizzaDto> CreateAsync(CreatePizzaDto dto);
-        Task<PizzaDto> UpdateAsync(UpdatePizzaDto dto);
+        Task<PizzaDto> UpdateAsync(int id, UpdatePizzaDto dto);
         Task<bool> DeleteAsync(int id);
         Task<int> CountAsync();
     }
